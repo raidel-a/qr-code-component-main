99a79e64d3c2365516539fd1d4541d3427239d3d
raihven <raihven@protonmail.com>
2023-10-26 23:20:48 -0400

    init

---
+++ QRcode.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import QRCode from 'react-qr-code';
+
+function QRcode {
+  <QRCode value="Hello, World!" />,
+  document.getElementById('root')
+}
